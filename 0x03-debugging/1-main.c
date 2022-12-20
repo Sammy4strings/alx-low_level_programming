30a2d831975c84751e1f93be3e0f79f2ce0c638e
sammy4strings <sammy4string@gmail.com>
2022-12-20 00:16:24 -0800

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 /*
  * while (i < 10)
@@ -17,7 +16,7 @@ int main(void)
  * putchar(i);
  * }
  */
-	printf("Infinite loop avoided! \\\n");
+	printf("Infinite loop avoided! \\0\n");
 
 	return (0);
 }
